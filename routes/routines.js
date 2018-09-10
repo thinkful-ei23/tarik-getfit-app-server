94a4f3af8d0dd58072cbc2ef8dc62b55d366274a
Tarik Desire <tprdesire@gmail.com>
2018-09-10 13:48:33 -0400

    Added and mounted routines route with GET endpoint to retrieve all routines

---
+++ routes/routines.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Routine = require('../models/routine');
+const Exercise = require('../models/exercise');
+const Tag = require('../models/tag');
+
+const Router = express.Router();
+
+Router.get('/', (req, res, next) => {
+  let filter = {};
+  const { searchTerm } = req.query;
+  const { tagId } = req.query;
+  
+  if (searchTerm) {
+    filter.title = searchTerm;
+  }
+  if (tagId) {
+    filter.tags = tagId;
+  }
+
+  return Routine.find(filter)
+    .sort({ updatedAt: 'desc'})
+    .populate('exercises', 'name sets reps')
+    .populate('tags', 'name')
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+module.exports = Router;
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
+const routinesRouter = require('./routes/routines');
+
 const app = express();
 
 app.use(
@@ -21,6 +23,24 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+app.use('/api/routines', routinesRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    const errBody = Object.assign({}, err, { message: err.message });
+    res.status(err.status).json(errBody);
+  } else {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 
 function runServer(port = PORT) {
   const server = app
