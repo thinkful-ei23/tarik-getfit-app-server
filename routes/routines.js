bc64d5b99538ee3b5f06e15364e116bef4244b62
Tarik Desire <tprdesire@gmail.com>
2018-09-17 12:31:33 -0400

    Continued refactor

--- routes/routines.js
+++ routes/routines.js
@@ -4,8 +4,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const Routine = require('../models/routine');
-const Exercise = require('../models/exercise');
-const Tag = require('../models/tag');
+// const Exercise = require('../models/exercise');
+// const Tag = require('../models/tag');
 
 const Router = express.Router();
 
@@ -118,14 +118,16 @@ Router.post('/', (req, res, next) => {
         err.status = 400;
         return next(err);
       }
-
-      if (exercise.userId !== req.user.id) {
-        const err = new Error('This `exercise` does not belong to the user');
-        err.status = 400;
-        return next(err);
-      }
     });
   }
+
+  const mappedEx = newRoutine.exercises.map(exercise => {
+    return Object.assign({}, exercise, {
+      userId
+    });
+  });
+
+  newRoutine.exercises = mappedEx;
 
   // if (newRoutine.tags) {
   //   if (!Array.isArray(newRoutine.tags)) {
@@ -161,89 +163,116 @@ Router.post('/', (req, res, next) => {
     });
 });
 
-Router.put('/:id', (req, res, next) => {
-  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id ` +
-      `(${req.body.id}) must match`);
-    console.error(message);
-    return res.status(400).json({message});
-  }
+// Router.put('/:id', (req, res, next) => {
+//   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+//     const message = (
+//       `Request path id (${req.params.id}) and request body id ` +
+//       `(${req.body.id}) must match`);
+//     console.error(message);
+//     return res.status(400).json({message});
+//   }
 
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    const err = new Error('The `endpoint id` is not valid');
-    err.status = 400;
-    return next(err);
-  }
+//   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+//     const err = new Error('The `endpoint id` is not valid');
+//     err.status = 400;
+//     return next(err);
+//   }
 
-  const { id } = req.params;
+//   const { id } = req.params;
 
-  const updateObj = {};
-  const updatableFields = ['title', 'description', 'exercises', 'tags'];
-  updatableFields.forEach(field => {
-    if (req.body[field]) {
-      updateObj[field] = req.body[field];
-    }
-  });
+//   const updateObj = {};
+//   const userId = req.user.id;
+//   const updatableFields = ['title', 'description', 'exercises', 'tags'];
+//   updatableFields.forEach(field => {
+//     if (req.body[field]) {
+//       updateObj[field] = req.body[field];
+//     }
+//   });
 
-  if (!updateObj.title) {
-    const err = new Error('Missing `title` in request body');
-    err.status = 400;
-    return next(err);
-  }
+//   if (!updateObj.title) {
+//     const err = new Error('Missing `title` in request body');
+//     err.status = 400;
+//     return next(err);
+//   }
 
-  if (!updateObj.exercises) {
-    updateObj.exercises = [];
-  }
+//   if (!updateObj.exercises) {
+//     updateObj.exercises = [];
+//   }
 
-  if (!updateObj.tags) {
-    updateObj.tags = [];
-  }
+//   if (!updateObj.tags) {
+//     updateObj.tags = [];
+//   }
 
-  if (updateObj.exercises) {
-    if (!Array.isArray(updateObj.exercises)) {
-      const err = new Error('`exercises` is not an array');
-      err.status = 400;
-      return next(err);
-    }
+//   if (updateObj.exercises) {
+//     if (!Array.isArray(updateObj.exercises)) {
+//       const err = new Error('`exercises` is not an array');
+//       err.status = 400;
+//       return next(err);
+//     }
 
-    updateObj.exercises.forEach(exercise => {
-      if (!mongoose.Types.ObjectId.isValid(exercise)) {
-        const err = new Error('The `exercise` array contains an invalid `id`');
-        err.status = 400;
-        return next(err);
-      }
-    });
-  }
+//     updateObj.exercises.forEach(exercise => {
+//       if (typeof exercise !== 'object') {
+//         const err = new Error('The `exercise` array must consist of objects');
+//         err.status = 400;
+//         return next(err);
+//       }
 
-  if (updateObj.tags) {
-    if (!Array.isArray(updateObj.tags)) {
-      const err = new Error('`tags` is not an array');
-      err.status = 400;
-      return next(err);
-    }
+//       if (!exercise.name) {
+//         const err = new Error('Missing `name` for object in `exercise` array');
+//         err.status = 400;
+//         return next(err);
+//       }
 
-    updateObj.tags.forEach(tag => {
-      if (!mongoose.Types.ObjectId.isValid(tag)) {
-        const err = new Error('The `tags` array contains an invalid `id`');
-        err.status = 400;
-        return next(err);
-      }
-    });
-  }
+//       if (typeof exercise.sets !== 'number') {
+//         const err = new Error('`sets` is not a number for object in `exercise` array');
+//         err.status = 400;
+//         return next(err);
+//       }
 
-  return Routine.findByIdAndUpdate(id, updateObj, {new: true})
-    .then(result => {
-      if (result) {
-        res.json(result);
-      } else {
-        next();
-      }
-    })
-    .catch(err => {
-      next(err);
-    });
-});
+//       if (typeof exercise.reps !== 'number') {
+//         const err = new Error('`reps` is not a number for object in `exercise` array');
+//         err.status = 400;
+//         return next(err);
+//       }
+//     });
+//   }
+  
+//   const mappedEx = updateObj.map(exercise => {
+//     if (!exercise.userId) {
+
+//     }
+//   })
+
+//   if (updateObj.tags) {
+//     if (!Array.isArray(updateObj.tags)) {
+//       const err = new Error('`tags` is not an array');
+//       err.status = 400;
+//       return next(err);
+//     }
+
+//     updateObj.tags.forEach(tag => {
+//       if (!mongoose.Types.ObjectId.isValid(tag)) {
+//         const err = new Error('The `tags` array contains an invalid `id`');
+//         err.status = 400;
+//         return next(err);
+//       }
+//     });
+//   }
+
+//   return Exercise.create()
+
+//   return Routine.findByIdAndUpdate(id, updateObj, {new: true})
+//     .then(result => {
+//       if (result) {
+//         res.json(result);
+//       } else {
+//         next();
+//       }
+//     })
+//     .catch(err => {
+//       next(err);
+//     });
+// });
 
 Router.delete('/:id', (req, res, next) => {
   const { id } = req.params;
